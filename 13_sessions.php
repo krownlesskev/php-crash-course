2dd655ef5c030558c45cfbd815b0038eb8e3ea2b
Kevin Sana <kevin.sana@outlook.com>
2022-05-24 01:19:16 -0500

    sessions

---
+++ 13_sessions.php
@@ -0,0 +1,35 @@
+<?php
+
+/* Sessions store information (in vars) to
+ to be used across multiple pages. Unlike cookies 
+ sessions are stored on the server.
+*/
+
+session_start();
+
+if (isset($_POST['submit'])) {
+    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    $password = $_POST['password'];
+
+    if ($username == 'kevin' && $password == 'password') {
+        $_SESSION['username'] = $username;
+        header('Location: /php-crash/extras/dashboard.php');
+    } else {
+        echo 'Incorrect Credentials';
+    }
+}
+?>
+
+
+<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="POST">
+    <div>
+        <label for="username">Username: </label>
+        <input type="text" name="username">
+    </div>
+    <div>
+        <label for="password">Password </label>
+        <input type="password" name="password">
+    </div>
+    <input type="submit" value="Submit" name="submit">
+</form>
