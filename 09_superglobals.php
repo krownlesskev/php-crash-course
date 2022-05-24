03586c673b2b944c2a4cbf45357711e133347224
Kevin Sana <kevin.sana@outlook.com>
2022-05-24 00:11:33 -0500

    superglobals

---
+++ 09_superglobals.php
@@ -0,0 +1,14 @@
+<?php
+
+// Built in variables that are always available in all scopes
+
+/*
+$_GET - Contains info about variables passed through a URL or form
+$_POST - Contains info about variables pass through a form
+$_COOKIE - Contains info about variables passed through a cookie
+$_SESSION - Contains info about vars passed through a session.
+$_SERVER - Contains info about the server enviroment
+$_ENV - info about env variables
+$_FILES - info about files uploaded to the script
+$REQUEST - info about vars passed through the form or URL.
+*/
