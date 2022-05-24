41ee7e12fa0ff9af616a4553a1a01f3a7596d241
Kevin Sana <kevin.sana@outlook.com>
2022-05-23 23:40:05 -0500

    string functions

---
+++ 08_string_functions.php
@@ -0,0 +1,48 @@
+<?php
+
+$string = 'Hello World';
+
+// Get the length of a string
+// echo strlen($string);
+
+// find the position of the first occurence of a substring in a string
+// echo strpos($string, 'o');
+
+// find the position of the last occurence of a substring in a string
+// echo strrpos($string, 'o');
+
+// Reverse a string
+// echo strrev($string);
+
+// Convert all characters to lowercase
+// echo strtolower($string);
+
+// Convert all characters to uppercase
+// echo strtoupper($string);
+
+// Uppercase the first character of each word
+// echo ucwords($string);
+
+// String replace
+// echo str_replace('World', 'Everyone', $string);
+
+// Return portion of a string specified by the offset and length
+// echo substr($string, 0, 5);
+// echo substr($string, 5);
+
+// Starts with
+// if (str_starts_with($string, 'Hello')) {
+//     echo 'YES';
+// }
+
+// // Ends with
+// if (str_ends_with($string, 'World')) {
+//     echo 'YES';
+// }
+
+$string2 = '<h1>Hello</h1>';
+
+// echo htmlspecialchars($string2);
+
+printf('%s likes to %s', 'Kevin', 'code');
+printf('1+1=%d', 1 + 1);
